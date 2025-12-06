bcffd1359b9d5961bb48181769e1b0b468167c8c
Daniel <daniel.diaz.m@uni.pe>
2025-12-05 20:23:46 -0500

    Entrega 1

---
+++ Include/curso.h
@@ -0,0 +1,16 @@
+#ifndef CURSO_H
+#define CURSO_H
+
+#include <string>
+using namespace std;
+
+class Curso {
+private:
+    int id;
+    string nombre;
+public:
+    Curso(int id, const string& nombre) : id(id), nombre(nombre) {}
+    int getId() const { return id; }
+    string getNombre() const { return nombre; }
+};
+#endif
